196854efc686ddd3aebaca8b29c33ef578a53569
mogy-h <68378904+mogy-h@users.noreply.github.com>
2021-11-19 15:22:51 +0900

    various styling in react

--- src/components/CssModules.jsx
+++ src/components/CssModules.jsx
@@ -4,7 +4,7 @@ export const CssModules = () => {
   return (
     <div className={classes.container}>
       <p className={classes.title}>CSS Modules</p>
-      <button className={classes.button}>Yay</button>
+      <button className={classes.button}>FIGHT</button>
     </div>
   );
 };
